c78ff18dad898f698b9fa5bb052091733a210c79
Matthew McKney <mrmckney@gmail.com>
2021-09-15 14:51:46 -0400

    added localhost

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+require('dotenv/config')
+
+
+const app = express()
+const router = express.Router()
+app.use(cors())
+app.use(express.json())
+app.use("/", router)
+
+mongoose
+  .connect(process.env.DB_CONNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen('5000', () => {
+      console.log('our app is listening on port 5000')
+    })
+  })
+  .catch(err => console.log(err))
+
